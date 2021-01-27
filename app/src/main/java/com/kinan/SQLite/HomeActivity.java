a60d001e13c9e62241ff2b13541344aa4d7031f2
kinancy <lambakinancy11@gmail.com>
2021-01-28 00:20:26 +0800

    TugaskelompokAplikasiMobile

---
+++ app/src/main/java/com/kinan/SQLite/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.kinan.SQLite;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button btnHome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnHome = (Button) findViewById(R.id.btnhome);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent open = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(open);
+            }
+        });
+    }
+}
